e1b70991370f74d6a24a71a3d98a216c537e9f7e
Andrew Novikov <andrew.nvkv@gmail.com>
2022-02-03 11:54:02 +0300

    task 2

--- ex2/__init__.py
+++ ex2/__init__.py
@@ -10,10 +10,20 @@ def benchmark(num):
     :param num: число итераций
     :return: функцию обёртку
     """
-    def wrapper(func):
-        # put your code here
-        pass
-    return wrapper
+    def decorator(func):
+        import time
+        def wrapper(*args, **kwargs):
+            fetch_timetable = []
+            for i in range(num):
+                start_time = time.time()
+                func(*args, **kwargs)
+                end_time = time.time()
+                fetch_timetable.append(end_time - start_time)
+            for i in range(len(fetch_timetable)):
+                print("Fetch #{}, {:.3} seconds".format(i+1, fetch_timetable[i]))
+            print("Average fetch time: {:.3} seconds".format(sum(fetch_timetable)/len(fetch_timetable)))
+        return wrapper
+    return decorator
 
 
 @benchmark(CALL_COUNT)
